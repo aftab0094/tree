fabb0832b6fff6849a517b8018d46a2750a16af2
AlHind AHITTA <tipsgamer596@gmail.com>
2026-05-08 10:19:39 +0530

    tree alternative because the windows one is a piece of shit

---
+++ tree.py
@@ -0,0 +1,77 @@
+import os
+import pyperclip
+import time
+
+def is_dir(path):
+    try:
+        with open(path) as f:
+            f.close()
+        return False
+    except Exception as e:
+        # print(e)
+        return True
+
+def is_child(dirc):
+    os.chdir(dirc)
+    if os.listdir() != []:
+        os.chdir("..")
+        return True
+    os.chdir("..")
+    return False
+
+def get_child(dirc):
+    os.chdir(dirc)
+    a = os.listdir()
+    os.chdir("..")
+    return a
+
+def str_length(string):
+    a = 0
+    for char in string:
+        a += 1
+    return a
+
+def format_child(dirc):
+    # ["child", ...] -> ["   child",...]
+    # " " X dirc.length + " --- ".length
+    child = get_child(dirc)
+    temp_string = (str_length(dirc) + 5) * " "
+    print(temp_string)
+    for index, path in enumerate(child):
+        if index == 0:
+            continue
+        child[index] = "\n" + temp_string + path
+    return child
+
+def print_child(dirc):
+    fmt_list = format_child(dirc)
+    fmt_string = f"{dirc} --- "
+    for index, path in enumerate(fmt_list):
+        fmt_string += path
+    print(fmt_string)
+    return fmt_string
+
+ls_cur = os.listdir()
+
+def main():
+    global tree
+    tree = ""
+    for path in ls_cur:
+        if is_dir(path):
+            # print(f"{path} is dir")
+            if not is_child(path):
+                print(path)
+                continue
+            # print(f"{path} has child")
+            # print(f"{path} --- ", end="")
+            tree += "\n" + print_child(path)
+        else:
+            print(path)
+            tree += "\n" + path
+        # print("iterated")
+
+if __name__ == "__main__":
+    main()
+    perm = input("Do you want to copy this to clipboard?(y/n) ")
+    if perm == "y":
+        pyperclip.copy(tree)
